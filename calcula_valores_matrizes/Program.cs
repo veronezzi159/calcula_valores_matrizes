78e4436a2507fdb794d29dbe6f6b3089a17a0e5f
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-25 00:04:34 -0300

    funções

--- calcula_valores_matrizes/Program.cs
+++ calcula_valores_matrizes/Program.cs
@@ -1,36 +1,131 @@
-﻿int qtd_linhas = 0,  qtd_colunas = 0;
+﻿int qtd_linhas = 0,  qtd_colunas = 0, escolha = 0;
 
-do
+float[,] matriz1 = new float[qtd_linhas, qtd_colunas], matriz2 = new float[qtd_linhas, qtd_colunas], matriz_soma = new float[qtd_linhas, qtd_colunas];
+float[,] matriz_sub = new float[qtd_linhas, qtd_colunas];
+float[,] matriz_div = new float[qtd_linhas, qtd_colunas];
+float[,] matriz_mult = new float[qtd_linhas, qtd_colunas];
+
+void imprimirMatriz(float[,] matriz, string titulo)
 {
-    Console.Write("Digite a quantidade de colunas das matrizes: ");
-    qtd_colunas = int .Parse(Console.ReadLine());
-    Console.Write("\nDigite a quantidade de linhas das matrizes: ");
-    qtd_linhas = int .Parse(Console.ReadLine());    
-} while (qtd_linhas < 1 && qtd_colunas < 1);
-
-int[,] matriz1 = new int[qtd_linhas, qtd_colunas], matriz2 = new int[qtd_linhas, qtd_colunas], matriz_soma = new int[qtd_linhas, qtd_colunas];
-int[,] matriz_sub = new int[qtd_linhas, qtd_colunas];
-int[,] matriz_div = new int[qtd_linhas, qtd_colunas];
-int[,] matriz_mult = new int[qtd_linhas, qtd_colunas];
-
-for (int linhas = 0; linhas < qtd_linhas; linhas++)
-{
-    for (int colunas = 0;  colunas < qtd_colunas; colunas++)
+    Console.WriteLine($"\n {titulo}");
+    for (int linhas = 0; linhas < qtd_linhas; linhas++)
     {
-        matriz1[linhas, colunas] = new Random().Next(0,101);
-        matriz2[linhas, colunas] = new Random().Next(0, 101);
-        matriz_soma[linhas, colunas] = matriz1[linhas,colunas] + matriz2[linhas,colunas];
-        matriz_sub[linhas, colunas] = matriz1[linhas,colunas] - matriz2[linhas,colunas];
-        matriz_div[linhas, colunas] = matriz1[linhas,colunas] / matriz2[linhas,colunas];
-        matriz_mult[linhas, colunas] = matriz1[linhas,colunas] * matriz2[linhas,colunas];
+        Console.WriteLine();
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            Console.Write($"{matriz[linhas,colunas]} ");
+        }
     }
-}
-
-for (int linhas = 0; linhas  < qtd_linhas; linhas++)
-{
     Console.WriteLine();
-    for (int colunas = 0; colunas < qtd_colunas; colunas++)
+}
+void menu()
+{
+    Console.WriteLine("Escolha qual operação deseja realizar");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Multiplicação");
+    Console.WriteLine("4 - Divisão");
+    Console.WriteLine("digite qualque numero pra encerrar");
+    escolha = int.Parse( Console.ReadLine() );
+}
+void encerrar()
+{
+    Console.WriteLine("Obrigado! Encerrando");
+    Console.ReadLine();
+}
+
+void escolher_dimensoes()
+{
+    do
     {
-        Console.Write($"linha: {linhas} coluna: {colunas}, soma:{ matriz_soma[linhas,colunas]}, subtração: {matriz_sub[linhas,colunas]}, multiplicação: {matriz_mult[linhas, colunas]}, divisão: {matriz_div[linhas,colunas]}\n");
+        Console.Write("Digite a quantidade de colunas das matrizes: ");
+        qtd_colunas = int.Parse(Console.ReadLine());
+        Console.Write("\nDigite a quantidade de linhas das matrizes: ");
+        qtd_linhas = int.Parse(Console.ReadLine());
+    } while (qtd_linhas < 1 && qtd_colunas < 1);
+}
+
+void sortear_matriz(float[,] matriz)
+{
+    for (int linhas= 0; linhas < qtd_linhas; linhas++)
+    {
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            matriz[linhas, colunas] = new Random().Next(0, 101);
+        }
     }
 }
+void somar_matrizes(float[,] soma, float[,] matriz1, float[,] matriz2)
+{
+    for (int linhas = 0; linhas < qtd_linhas; linhas++)
+    {
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            soma[linhas, colunas] = matriz1[linhas, colunas] + matriz2[linhas, colunas];
+        }
+    }
+}
+void subtrair_matrizes(float[,] sub, float[,] matriz1, float[,] matriz2)
+{
+    for (int linhas = 0; linhas < qtd_linhas; linhas++)
+    {
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            sub[linhas, colunas] = matriz1[linhas, colunas] - matriz2[linhas, colunas];
+        }
+    }
+}
+void multiplicar_matrizes(float[,] mult, float[,] matriz1, float[,] matriz2)
+{
+    for (int linhas = 0; linhas < qtd_linhas; linhas++)
+    {
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            mult[linhas, colunas] = matriz1[linhas, colunas] * matriz2[linhas, colunas];
+        }
+    }
+}
+void dividir_matrizes(float[,] div, float[,] matriz1, float[,] matriz2)
+{
+    for (int linhas = 0; linhas < qtd_linhas; linhas++)
+    {
+        for (int colunas = 0; colunas < qtd_colunas; colunas++)
+        {
+            if (matriz2[linhas, colunas] != 0)
+            {
+                div[linhas, colunas] = matriz1[linhas, colunas] / matriz2[linhas, colunas];
+            }
+            else
+            {
+                div[linhas, colunas] = float.NaN;
+            }
+        }
+    }
+}
+
+menu();
+sortear_matriz(matriz1);
+sortear_matriz(matriz2);
+
+imprimirMatriz(matriz1, "Matriz 1");
+
+imprimirMatriz(matriz2, "Matriz 2");
+
+switch (escolha)
+{
+    case 1: somar_matrizes(matriz_soma, matriz1, matriz2);
+            imprimirMatriz(matriz_soma, "Matriz Soma: ");
+        break;
+    case 2: subtrair_matrizes(matriz_sub, matriz1, matriz2);
+            imprimirMatriz(matriz_sub, "Matriz Subtração");
+        break;
+    case 3: multiplicar_matrizes(matriz_mult, matriz1, matriz2);
+            imprimirMatriz(matriz_mult, "Matriz Multiplicada");
+        break;
+    case 4: dividir_matrizes(matriz_div, matriz1, matriz2);
+            imprimirMatriz(matriz_div, "Matriz Dividida");
+        break;
+    default: encerrar();
+        break;
+}
+
